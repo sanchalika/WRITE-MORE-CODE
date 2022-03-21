68f1d0bee2f9bc0c3f9d676d305db6c5b905786b
sanchalika <sanchalika2019@gmail.com>
2022-03-21 10:14:55 +0530

    c

--- CSES/weird_algorithm.cpp
+++ CSES/weird_algorithm.cpp
@@ -1,24 +1,21 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    // vector<int> v;
-    int n;
+    long long n;
     cin >> n;
-
     while (n != 1)
     {
         cout << n << " ";
         if (n % 2 == 0)
         {
-            n = n / 2;
+            n /= 2;
         }
         else
         {
-            n = (n * 3) + 1;
+            n = 3 * n + 1;
         }
     }
     cout << 1;
-    return 0;
 }
