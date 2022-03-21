f7aa4752d28f09e6572e77b402a36113f13cd2dc
sanchalika <sanchalika2019@gmail.com>
2022-03-21 20:13:31 +0530

    commit

--- CSES/subarray_sums_I.cpp
+++ CSES/subarray_sums_I.cpp
@@ -1,25 +1,46 @@
-#include <iostream>
+
+#include <bits/stdc++.h>
 using namespace std;
+const int mod = 1e9 + 7;
+#define ll long long
+const int mx = 2e5 + 5;
 int main()
 {
-    int n, x;
-    cin >> n >> x;
-    int res = 0;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll n, sum;
+    cin >> n >> sum;
+    vector<ll> v(n), presum(n, 0);
+    for (int i = 0; i < n; ++i)
+        cin >> v[i];
+    presum[0] = v[0];
+    for (int i = 1; i < n; ++i)
+        presum[i] = presum[i - 1] + v[i];
+
+    ll j = 0, cnt = 0;
+    for (int i = 0; i < n; ++i)
     {
-        cin >> arr[i];
-    }
-    
-    for (int i = 0; i < n; i++)
-    {
-        int sum = 0;
-        for (int k = i; k < n; k++)
+        if (presum[i] > sum)
         {
-            sum += arr[k];
-            if (sum == x)
-                res++;
+            while (j < i)
+            {
+                int p = presum[i] - presum[j];
+                if (p == sum)
+                {
+                    cnt++;
+                    break;
+                }
+                else if (p < sum)
+                    break;
+                j++;
+            }
+        }
+        else if (presum[i] == sum)
+        {
+            cnt++;
         }
     }
-    cout << res << endl;
+    cout << cnt << endl;
+    return 0;
 }
---
+++ Recursion/lec3_q_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool linearSearch(int arr[], int size, int k)
+{
+
+    if (arr[0] == k)
+    {
+        return true;
+    }
+    else
+    {
+        bool remainingPart = linearSearch(arr + 1, size - 1, k);
+        return remainingPart;
+    }
+}
+int main()
+{
+
+    int arr[5] = {3, 5, 1, 2, 6};
+    int size = 5;
+    int key = 2;
+    bool ans = linearSearch(arr, size, key);
+    if (ans)
+    {
+        cout << "present" << endl;
+    }
+    else
+    {
+        cout << "Absent" << endl;
+    }
+
+    return 0;
+}
