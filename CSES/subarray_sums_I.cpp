399b7cfe1aeb8a4767a8fe6e91bb8ec742024a83
sanchalika <sanchalika2019@gmail.com>
2022-03-21 17:14:44 +0530

    commit

---
+++ CSES/subarray_sums_I.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+    int res = 0;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int k = i; k < n; k++)
+        {
+            sum += arr[k];
+            if (sum == k)
+                res++;
+        }
+    }
+    cout << res << endl;
+}
